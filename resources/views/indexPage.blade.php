da758920b7f41ec4d769a6d2e2af8e8dd0575617
DmitryAlex2012 <tpmfd27@gmail.com>
2020-08-06 19:48:52 +0300

    Made "Logo" for index page. Begin make index page with "slick js".

--- resources/views/indexPage.blade.php
+++ resources/views/indexPage.blade.php
@@ -7,6 +7,24 @@
 
     <p class="myClass">My class</p>
 
+    <div class="container">
+
+        <?php for ($i = 0; $i<2; $i++ ): ?>
+            <div class="row">
+                <div class="cardImages col-md-4">
+                    Одна из трёх колонок
+                </div>
+                <div class="cardImages col-md-4">
+                    Одна из трёх колонок
+                </div>
+                <div class="cardImages col-md-4">
+                    Одна из трёх колонок
+                </div>
+            </div>
+        <?php endfor; ?>
+
+    </div>
+
 @endsection
 
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,12 +8,14 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="{{ asset('css/index.css') }}" rel="stylesheet" >
+{{--    <link href="{{ asset('css/index.css') }}" rel="stylesheet" >--}}
     <title></title>
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?php echo action('IndexController@index'); ?>">Navbar</a>
+{{--    <a class="navbar-brand" href="<?php echo action('IndexController@index'); ?>">Navbar</a>--}}
+    <a class="navbar-brand" href="<?php echo action('IndexController@index'); ?>"><img src='{{ asset('images/1.jpg') }}' alt="100%"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
